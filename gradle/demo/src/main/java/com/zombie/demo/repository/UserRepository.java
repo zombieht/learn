de04cfbdd54169071673265b3cf3c20ebb604973
zombieht <zombie19931113@gmail.com>
2018-12-28 21:52:25 +0800

    1

--- gradle/demo/src/main/java/com/zombie/demo/repository/UserRepository.java
+++ gradle/demo/src/main/java/com/zombie/demo/repository/UserRepository.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+
     @EntityGraph(attributePaths = "authorities")
     User findByUsername(String s);
 }
